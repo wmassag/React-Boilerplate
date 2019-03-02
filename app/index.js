e3ceebf222989d8dc40c795a432b553068f3605a
Wenzel Massag <mail@wmassag.de>
2019-03-02 16:18:56 +0100

    Init

---
+++ app/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import ReactDOM from "react-dom"
+const app = document.getElementById('app')
+
+require("./style.scss")
+
+//import Layout from "./fragments/pages/Layout/"
+
+ReactDOM.render(
+  <div>Boilerplate</div>,
+  app
+);
